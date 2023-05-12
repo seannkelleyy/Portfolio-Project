33a530ea3fb0a39fbde7ca0d29ae5917638199ac
Sean Kelley <sean.kelley@neudesic.com>
2023-05-12 11:38:00 -0400

    Implemented goals, hobbies and projects sections.

--- Portfolio Project/src/App.jsx
+++ Portfolio Project/src/App.jsx
@@ -1,10 +1,16 @@
 import NameAndPhoto from './NameAndPhoto';
+import Goals from './Goals';
+import Hobbies from './Hobbies';
+import Projects from './Projects';
 
 function App() {
 
   return (
     <>
       <div><NameAndPhoto /></div>
+      <div><Goals /></div>
+      <div><Hobbies /></div>
+      <div><Projects /></div>
     </>
   )
 }

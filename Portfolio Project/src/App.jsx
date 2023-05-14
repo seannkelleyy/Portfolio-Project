583e5118e9315b68d9f2744371b7493bbca86880
Sean Kelley <sean.kelley@neudesic.com>
2023-05-14 14:36:15 -0400

    Finished CSS file.

--- Portfolio Project/src/App.jsx
+++ Portfolio Project/src/App.jsx
@@ -1,17 +1,21 @@
-import NameAndPhoto from './NameAndPhoto';
+import NameInfoBar from './NameInfoBar';
 import Goals from './Goals';
 import Hobbies from './Hobbies';
 import Projects from './Projects';
+import NameAndPhoto from './NameAndPhoto';
 
 function App() {
 
   return (
-    <>
-      <div><NameAndPhoto /></div>
-      <div><Goals /></div>
-      <div><Hobbies /></div>
-      <div><Projects /></div>
-    </>
+    <div className='page'>
+      <div><NameInfoBar /></div>
+      <div className='name-and-photo'><NameAndPhoto /></div>
+      <div className='grid-container-bottom'>
+        <div><Goals /></div>
+        <div><Projects /></div>
+        <div><Hobbies /></div>
+      </div>
+    </div>
   )
 }
 

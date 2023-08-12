ba514f97840760e785738250a8c734da130b78bf
Sean Kelley <sean.kelley@neudesic.com>
2023-08-12 11:43:06 -0400

    WIP

--- Portfolio-Project/src/assets/data/ProjectsData.jsx
+++ Portfolio-Project/src/assets/data/ProjectsData.jsx
@@ -2,17 +2,17 @@ export const ProjectsData = [
     {
         title: 'My first website',
         link: 'https://github.com/seannkelleyy/Website',
-        image: './src/assets/images/firstWebsite.png'
+        image: '../../images/firstWebsite.png'
 
     },
     {
         title: 'Mortgage Calculator',
         link: 'https://github.com/seannkelleyy/MortgageCalculator',
-        image: './src/assets/images/mortgageCalculator.png'
+        image: '../../images/mortgageCalculator.png'
     },
     {
         title: 'This Website',
         link: 'https://github.com/seannkelleyy/Portfolio-Project',
-        image: './src/assets/images/portfolioProject.png'
+        image: '../../images/portfolioProject.png'
     },
 ];

73179310496823e50ed69d293c83a24c43487b8c
Sean Kelley <sean.kelley@neudesic.com>
2023-09-27 11:10:51 -0400

    WIP

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-module.exports = {
-  root: 'src',
-  build: {
-    outDir: '../dist',
-    assetsDir: '/src/assets'
-  }
-}
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '',
+  build: {
+    outDir: '/dist',
+    assetsDir: 'src/assets'
+  }
 })

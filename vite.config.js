8abd344d326033c574355a3038e6ef872e2cc301
Sean Kelley <sean.kelley@neudesic.com>
2023-09-27 10:58:49 -0400

    WIP

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+module.exports = {
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    assetsDir: '/src/assets'
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react ()],
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split ('.').at (-1);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test (extType)) {
-            extType = 'img';
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-      },
-    },
-  },
-});
+  plugins: [react()],
+})

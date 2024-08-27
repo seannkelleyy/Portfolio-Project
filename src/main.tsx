b739fc44411451e16a27bd0a4eec01602dd7458b
seankelley <kelley.1102@buckeyemail.osu.edu>
2024-08-27 13:11:55 -0400

    Finishes small changes

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import ImageSlider from './components/imageSlider/ImageSlider.js'
 import Contact from './components/infoSections/Contact.js'
 import Projects from './components/infoSections/Projects.js'
 import Resume from './components/infoSections/Resume.js'
-import NavBar from './components/navigation/NavBar.js'
+import { NavBar } from './components/navigation/NavBar.js'
 import SideBar from './components/navigation/SideBar.js'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
